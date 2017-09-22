10a4eecda683e0aa57c8efe1f7c2eb40eade953a
u004ff <u004ff@gmail.com>
2017-09-22 11:29:02 +0900

    prototype

--- app/src/main/java/npaka/net/myapplication/info.java
+++ app/src/main/java/npaka/net/myapplication/info.java
@@ -5,18 +5,14 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class info extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_info);
     }
 
-    public void onInfomationButtonTapped(View view){
-        Intent intent = new Intent(this, info.class);
-        startActivity(intent);
-    }
     public void onTestButtonTapped(View view){
         Intent intent = new Intent(this, testmode.class);
         startActivity(intent);
