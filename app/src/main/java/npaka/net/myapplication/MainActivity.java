152dcf185f38fc075626cb538b1cd897fb815781
u004ff <u004ff@gmail.com>
2017-09-22 15:43:53 +0900

    002

--- app/src/main/java/npaka/net/myapplication/MainActivity.java
+++ app/src/main/java/npaka/net/myapplication/MainActivity.java
@@ -17,6 +17,10 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, info.class);
         startActivity(intent);
     }
+    public void onhelpButtonTapped(View view){
+        Intent intent = new Intent(this, help.class);
+        startActivity(intent);
+    }
     public void onTestButtonTapped(View view){
         Intent intent = new Intent(this, testmode.class);
         startActivity(intent);
