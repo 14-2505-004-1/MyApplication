ce1a3bc6a8ce507f9985353231611d16b87be0fa
u004ff <u004ff@gmail.com>
2017-09-22 16:47:58 +0900

    003

--- app/src/main/java/npaka/net/myapplication/testmode.java
+++ app/src/main/java/npaka/net/myapplication/testmode.java
@@ -4,13 +4,32 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 public class testmode extends AppCompatActivity {
+
+    int qCounter = 3;
+    int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_testmode);
+
+        TextView countlabel = (TextView)findViewById(R.id.number);
+        count++;
+        countlabel.setText(String.valueOf(count));
+    }
+
+    public void onrNextButtonTapped(View view){
+        if(count > 2){
+            Intent intent = new Intent(this, result.class);
+            startActivity(intent);
+        }else {
+            Intent intent = new Intent(this, testmode.class);
+            startActivity(intent);
+        }
+
     }
 
     public void onresultButtonTapped(View view){
