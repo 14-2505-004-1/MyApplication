873f1391011a2a023bc608eb88abec660d33520b
enpit028 <t035ff@yamaguchi-u.co.jp>
2017-09-22 16:10:00 +0900

    resultactivityの作成 testmodeの提出ボタンのontappedから飛ぶ

--- app/src/main/java/npaka/net/myapplication/testmode.java
+++ app/src/main/java/npaka/net/myapplication/testmode.java
@@ -1,7 +1,9 @@
 package npaka.net.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class testmode extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class testmode extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_testmode);
+    }
+
+    public void onresultButtonTapped(View view){
+        Intent intent = new Intent(this, result.class);
+        startActivity(intent);
     }
 }
